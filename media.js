5400c1e7ae0a8fc73c4b69f8139e70a020a48931
SimaraSc <simarascosta@hotmail.com>
2023-03-29 17:20:39 -0400

    adicionando exercício sobre média

--- media.js
+++ media.js
@@ -7,8 +7,8 @@
 
 //Joaozinho tirou 5 na primeira prova, e tirou 7 na segunda prova. Amédia para que Joao seja aprovado é: 6.
 
-let notaProva1 = 5;
-let notaProva2 = 3;
+let notaProva1 = 10;
+let notaProva2 = 2;
 
 let media = (notaProva1 + notaProva2) / 2;
 
